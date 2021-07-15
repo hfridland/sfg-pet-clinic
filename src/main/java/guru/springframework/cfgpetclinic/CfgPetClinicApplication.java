ef1f3adfb152e96fdb3a9492cd78d6740c9975bf
Haim Fridland <hfridland@shaw.ca>
2021-07-15 13:58:27 -0700

    Initial Project Add

---
+++ src/main/java/guru/springframework/cfgpetclinic/CfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.cfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/cfgpetclinic/CfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.cfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
